438b0f6f47615dd510f8d3cc8ab2404ddcd1d2ed
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-03 02:58:52 +0900

    feat : Token API

---
+++ src/main/java/kr/hhplus/be/server/api/token/TokenController.java
@@ -0,0 +1,19 @@
+package kr.hhplus.be.server.api.token;
+
+import java.time.LocalDateTime;
+import kr.hhplus.be.server.api.ApiResponse;
+import kr.hhplus.be.server.api.token.dto.TokenRequest;
+import kr.hhplus.be.server.api.token.dto.TokenResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+    @PostMapping("/tokens")
+    public ApiResponse<TokenResponse> d(@RequestBody TokenRequest request){
+        return ApiResponse.ok(TokenResponse.of("uuid", LocalDateTime.now(),true));
+    }
+
+}
