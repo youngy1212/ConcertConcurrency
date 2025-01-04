7e4e28c94cebaddcf7652aa70a90e63e13973504
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-05 06:17:45 +0900

    refactor : Builder 추가

--- src/main/java/kr/hhplus/be/server/api/concert/dto/ConcertDateResponse.java
+++ src/main/java/kr/hhplus/be/server/api/concert/dto/ConcertDateResponse.java
@@ -1,21 +1,26 @@
 package kr.hhplus.be.server.api.concert.dto;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
+
 
 @Getter
 public class ConcertDateResponse {
 
-    private Long concert_id;
-    private LocalDateTime concert_date;
+    private final Long concert_id;
+    private final LocalDateTime concert_date;
 
+    @Builder
     private ConcertDateResponse(Long concert_id, LocalDateTime concert_date) {
         this.concert_id = concert_id;
         this.concert_date = concert_date;
     }
 
     public static ConcertDateResponse of(Long concert_id, LocalDateTime concert_date) {
-        return new ConcertDateResponse(concert_id,concert_date);
+        return ConcertDateResponse.builder()
+                .concert_id(concert_id)
+                .concert_date(concert_date).build();
 
     }
 }
