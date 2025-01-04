7ad9178332f1d87abf909b836d92f80131706335
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-05 06:27:13 +0900

    feat : swagger 연결

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
+	// swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
