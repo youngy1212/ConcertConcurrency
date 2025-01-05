49f94bc6f9214c169ffdb4e084698c2afe7ab0f0
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-05 23:02:40 +0900

    feat : 에러처리

---
+++ src/main/java/kr/hhplus/be/server/domain/common/exception/CustomException.java
@@ -0,0 +1,31 @@
+package kr.hhplus.be.server.domain.common.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final HttpStatus httpStatus;
+    private final int errorCode;
+
+
+    public CustomException( HttpStatus httpStatus, int errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+        this.httpStatus = httpStatus;
+    }
+
+    public CustomException( HttpStatus httpStatus, String message) {
+        super(message);
+        this.errorCode = httpStatus.value();
+        this.httpStatus = httpStatus;
+    }
+
+    public CustomException(String message) {
+        super(message);
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+        this.errorCode = HttpStatus.BAD_REQUEST.value();// 기본값 설정
+    }
+
+}
---
+++ src/main/java/kr/hhplus/be/server/domain/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package kr.hhplus.be.server.domain.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        HttpStatus status = ex.getHttpStatus();
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getCode(),
+                status.getReasonPhrase(),
+                ex.getMessage()
+        );
+        return new ResponseEntity<>(errorResponse, status);
+    }
+}
