743bdf9e97b38d0b715fb6a4d7671792954f7b11
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-08 20:41:21 +0900

    feat : 대기열 토큰 발급 + 활성화 + 단위테스트

---
+++ src/main/java/kr/hhplus/be/server/domain/token/QueueTokenProcessor.java
@@ -0,0 +1,28 @@
+package kr.hhplus.be.server.domain.token;
+
+
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueueTokenProcessor {
+
+    private final QueueTokenReader queueTokenReader;
+
+    @Transactional
+    @Scheduled(fixedDelay = 1000) //1초 간격으로 실행
+    public void processActiveTokens(){
+
+        List<QueueToken> tokens = queueTokenReader.findTopNOrderByEnqueuedAt(QueueTokenStatus.PENDING, 10);
+
+        for(QueueToken token : tokens){
+            token.tokenActive(LocalDateTime.now());
+        }
+    }
+
+}
--- src/main/java/kr/hhplus/be/server/infrastructure/token/QueueTokenJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/token/QueueTokenJpaRepository.java
@@ -1,7 +1,14 @@
 package kr.hhplus.be.server.infrastructure.token;
 
+import jakarta.persistence.LockModeType;
+import java.util.List;
 import kr.hhplus.be.server.domain.token.QueueToken;
+import kr.hhplus.be.server.domain.token.QueueTokenStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface QueueTokenJpaRepository extends JpaRepository<QueueToken, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<QueueToken> findTop10ByStatusOrderByEnqueuedAtAsc(QueueTokenStatus status);
 }
