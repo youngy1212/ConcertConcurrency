bec47ffc748d70c51c0ef0866936421e56bb86ab
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-09 22:39:50 +0900

    test : TODO

--- src/test/java/kr/hhplus/be/server/aspect/AuthorizationAspectTest.java
+++ src/test/java/kr/hhplus/be/server/aspect/AuthorizationAspectTest.java
@@ -1,40 +1,6 @@
 package kr.hhplus.be.server.aspect;
 
 class AuthorizationAspectTest {
-
-//    @Mock
-//    private QueueTokenService queueTokenService;
-//
-//    @InjectMocks
-//    private AuthorizationAspect authorizationAspect;
-//
-//    @DisplayName("잘못된 인증정보로 예외 발생")
-//    @Test
-//    void testAuthorization_Exception() {
-//        // HttpServletRequest를 모킹합니다.
-//        HttpServletRequest request = mock(HttpServletRequest.class);
-//        when(request.getHeader("Authorization")).thenReturn(null);
-//
-//        // RequestContextHolder와 ServletRequestAttributes를 모킹합니다.
-//        ServletRequestAttributes attributes = mock(ServletRequestAttributes.class);
-//        when(attributes.getRequest()).thenReturn(request);
-//
-//        // RequestContextHolder에 모킹된 attributes를 설정합니다.
-//        RequestContextHolder.setRequestAttributes(attributes);
-//
-//        // JoinPoint를 모킹합니다.
-//        JoinPoint joinPoint = mock(JoinPoint.class);
-//
-//        // 예외가 발생하는지 검증합니다.
-//        CustomException exception = assertThrows(CustomException.class, () -> {
-//            authorizationAspect.Authorization(joinPoint);
-//        });
-//
-//        assertEquals("잘못 된 경로입니다.", exception.getMessage());
-//
-//        // RequestContextHolder를 정리합니다.
-//        RequestContextHolder.resetRequestAttributes();
-//
-//    }
+    //TODO: 나중에 다시 test
 
 }
