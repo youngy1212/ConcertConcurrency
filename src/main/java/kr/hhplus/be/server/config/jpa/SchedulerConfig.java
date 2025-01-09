894650064c2378c5cee0c81592f2d682c244686b
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-10 04:02:33 +0900

    feat : 콘서트 배치 처리

---
+++ src/main/java/kr/hhplus/be/server/config/jpa/SchedulerConfig.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+
+}
--- src/main/java/kr/hhplus/be/server/domain/token/QueueTokenScheduled.java
+++ src/main/java/kr/hhplus/be/server/domain/token/QueueTokenScheduled.java
@@ -10,13 +10,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class QueueTokenProcessor {
+public class QueueTokenScheduled {
+
 
     private final QueueTokenReader queueTokenReader;
 
     @Transactional
     @Scheduled(fixedDelay = 1000) //1초 간격으로 실행
     public void processActiveTokens(){
+        System.out.println("processActiveTokens 실행됨");
+
 
         List<QueueToken> tokens = queueTokenReader.findTopNOrderByEnqueuedAt(QueueTokenStatus.PENDING, 10);
 
