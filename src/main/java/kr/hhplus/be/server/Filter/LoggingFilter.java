64dff99bb225219a8991c8323643f0f617d6291a
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-15 04:28:04 +0900

    feat : 응답/요청에 대한 로그 Filter 처리

---
+++ src/main/java/kr/hhplus/be/server/Filter/LoggingFilter.java
@@ -0,0 +1,42 @@
+package kr.hhplus.be.server.Filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+
+        try {
+            filterChain.doFilter(wrappedRequest, wrappedResponse);
+        } finally {
+
+            String logMessage = HttpLogMessage.createInstance(
+                    wrappedRequest,
+                    wrappedResponse
+            ).toPrettierLog();
+
+            wrappedResponse.copyBodyToResponse();
+            logger.info(logMessage);
+        }
+    }
+
+}
