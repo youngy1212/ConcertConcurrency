70ce73838e8d2b3ca67e1a31b1dff3d974e59e4b
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-16 03:47:50 +0900

    refactor : 트랜잭션 오류 수정

--- src/main/java/kr/hhplus/be/server/domain/user/service/UserCommandService.java
+++ src/main/java/kr/hhplus/be/server/domain/user/service/UserCommandService.java
@@ -7,13 +7,15 @@ import kr.hhplus.be.server.domain.user.repository.UserCommand;
 import kr.hhplus.be.server.domain.user.service.dto.ChargeDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserCommandService {
 
-    private UserCommand userCommand;
+    private final UserCommand userCommand;
 
+    @Transactional
     public ChargeDto chargePoint(Long userId, Long amount) {
 
         if (amount == null || amount <= 0) {
