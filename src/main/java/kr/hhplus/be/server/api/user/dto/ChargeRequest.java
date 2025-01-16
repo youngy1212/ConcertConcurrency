115831d9a3fc7e0a40f997242c9c0b3aed7f6bb9
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2025-01-16 21:58:35 +0900

    refactor : 책임 변경 (TODO)

--- src/main/java/kr/hhplus/be/server/api/user/dto/ChargeRequest.java
+++ src/main/java/kr/hhplus/be/server/api/user/dto/ChargeRequest.java
@@ -1,9 +1,34 @@
 package kr.hhplus.be.server.api.user.dto;
 
+import kr.hhplus.be.server.domain.common.exception.CustomException;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ChargeRequest {
     private Long userId;
     private Long amount;
+
+    @Builder
+    private ChargeRequest(Long userId, Long amount) {
+        this.userId = userId;
+        this.amount = amount;
+    }
+
+    private static void validateAmount(Long amount) {
+        if (amount == null || amount <= 0) {
+            throw new CustomException("amount는 0보다 큰 양수여야 합니다.");
+        }
+    }
+
+    public static ChargeRequest toRequest(Long userId, Long amount) {
+        validateAmount(amount);
+        return ChargeRequest.builder()
+                .userId(userId)
+                .amount(amount)
+                .build();
+    }
+
 }
